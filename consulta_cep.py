917c3f2b675e15a1237d10c1a8c48a34fec81cae
carolHorowitz <carolvet.horowitz@gmail.com>
2022-11-29 21:40:11 -0300

    alteração da primeira condicional referente ao input do usuário

--- consulta_cep.py
+++ consulta_cep.py
@@ -2,20 +2,25 @@ import requests
 
 def main():
 
-    print('##################')
-    print('---Consulta CEP---')
-    print('##################')
+    print('###############################')
+    print('---Bem vindo ao CONSULTA CEP---')
+    print('###############################')
 
-    cep = input('Digite o CEP desejado: ')
+    # usuário digita o CEP para consulta
+    cep = input('Digite o CEP desejado, sem pontos ou traços: ')
 
-    if len(cep) != 8:
-        print('Digite um CEP válido!')
-        exit()
+    # enquanto o CEP fornecido não apresentar 8 digitos, aparecerá a mensagem de erro seguida de uma nova solicitação de CEP
+    while len(cep) != 8:
+        print('Por favor, digite um CEP com 8 dígitos')
+        cep = input('Digite o CEP desejado, sem pontos ou traços: ')
 
-    request = requests.get('https://viacep.com.br/ws/{}/json/' .format(cep))
+    # caso o CEP informado contenha 8 digitos, será feita uma requisão HTTP a webservice
+    requisicao = requests.get('https://viacep.com.br/ws/{}/json/' .format(cep))
 
-    dados_endereco = request.json()
+    # Os dados do CEP (caso seja válido) retornarão no formato json e serão atribuídos a variável dados_endereco  
+    dados_endereco = requisicao.json()
 
+    # caso o CEP seja encontrado, serão exibidos os dados referentes a ele
     if 'erro' not in dados_endereco:
         print('---- CEP encontrado! ----')
 
@@ -26,17 +31,22 @@ def main():
         print('CIDADE: {}'.format(dados_endereco['localidade']))
         print('ESTADO: {}'.format(dados_endereco['uf']))
 
+    # caso o CEP não seja válido, será exibida uma mensagem mde erro
     else:
         print('CEP inválido.\n')
 
-        opcao = int(input('Deseja realizar nova consulta? Digite 1 para SIM e 2 para SAIR.'))
 
-        if opcao == 1:
-            main()
+    opcao = int(input('Deseja realizar nova consulta? Digite 1 para CONTINUAR e 2 para SAIR.'))
 
-        else:
-            print('Saindo do sistema!')
+    # caso o usuário opte por continuar a consulta, a função será executada novamente
+    if opcao == 1:
+        main()
 
+    else:
+        print('------------------------------------------')
+        print('O sistema está sendo finalizado. Obrigada!')
+
+# caso a função solicitada tenha o nome main, será executada
 if __name__ == '__main__':
     main()
 
